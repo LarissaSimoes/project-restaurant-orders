66c91c7727138b6e9dc336619067f11631ca0e6d
Larissa Simões <larissacarneiro2@hotmail.com>
2023-09-07 21:06:40 -0300

    req 2 new

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -5,26 +5,21 @@ import pytest
 
 # Req 2
 def test_dish():
-    # Test valid dish creation
     dish = Dish("Spaghetti Carbonara", 15.99)
     assert dish.name == "Spaghetti Carbonara"
     assert dish.price == 15.99
 
-    # Test __repr__
     assert repr(dish) == "Dish('Spaghetti Carbonara', R$15.99)"
 
-    # Test equality (__eq__)
     same_dish = Dish("Spaghetti Carbonara", 15.99)
     different_dish = Dish("Lasagna", 12.99)
 
     assert dish == same_dish
     assert dish != different_dish
 
-    # Test hash (__hash__)
     assert hash(dish) == hash(same_dish)
     assert hash(dish) != hash(different_dish)
 
-    # Test adding ingredient dependencies
     tomato = Ingredient("tomato")
     bacon = Ingredient("bacon")
     cheese = Ingredient("cheese")
@@ -35,27 +30,22 @@ def test_dish():
 
     assert dish.recipe == {tomato: 2, bacon: 3, cheese: 1}
 
-    # Test get_restrictions
     assert dish.get_restrictions() == {
         Restriction.ANIMAL_DERIVED,
         Restriction.ANIMAL_MEAT,
     }
 
-    # Test get_ingredients
     assert dish.get_ingredients() == {tomato, bacon, cheese}
 
-    # Test invalid dish creation
     with pytest.raises(TypeError):
         Dish("Invalid Dish", "invalid_price")
 
     with pytest.raises(ValueError):
         Dish("Invalid Dish", -10.0)
 
-    # Test accessing quantity of ingredient
     assert dish.recipe.get(tomato) == 2
     assert dish.recipe.get(bacon) == 3
     assert dish.recipe.get(cheese) == 1
 
-    # Test that accessing a non-existent ingredient returns None
     mushroom = Ingredient("mushroom")
     assert dish.recipe.get(mushroom) is None
