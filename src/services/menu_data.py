7855c5a6cf2a185c1a6c9ab1f75ff1119b3cf5dd
Larissa Simões <larissacarneiro2@hotmail.com>
2023-09-07 21:16:20 -0300

    Req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,30 @@
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.load_menu_data(source_path)
+
+    def load_menu_data(self, source_path: str):
+        with open(source_path, mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                dish_name = row['dish']
+                dish_price = float(row['price'])
+                ingredient_name = row['ingredient']
+                ingredient_quantity = int(row['recipe_amount'])
+
+                dish = next(
+                    (d for d in self.dishes if d.name == dish_name),
+                    None
+                )
+                if dish is None:
+                    dish = Dish(dish_name, dish_price)
+                    self.dishes.add(dish)
+
+                ingredient = Ingredient(ingredient_name)
+                dish.add_ingredient_dependency(ingredient, ingredient_quantity)
